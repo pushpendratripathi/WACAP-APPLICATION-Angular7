695795c4bd85a051765a48e2df0d00366d6a1253
Pushpendra Tripathi <ujjwaltechi1@gmail.com>
2018-11-19 01:13:59 +0530

    updatedFiles

--- src/app/Wacap Application/wacap/wacap.component.ts
+++ src/app/Wacap Application/wacap/wacap.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-wacap',
   templateUrl: './wacap.component.html',
   styleUrls: ['./wacap.component.css']
 })
 export class WacapComponent implements OnInit {
-
+  langs: string[] = [
+    'Canada',
+    'USA',
+    'German',
+  ];
+  lengths: string[] = [
+    'duration',
+    'period',
+    'time'
+     ];
   constructor() { }
 
   ngOnInit() {
